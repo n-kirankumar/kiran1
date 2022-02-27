52df3f446fc7d768e7a1decfc823e37206e9d3c4
n-kirankumar <100504042+n-kirankumar@users.noreply.github.com>
2022-02-27 13:26:54 +0530

    Add files via upload
    divisibility python file

---
+++ 2 and 3 divisible till 15.py
@@ -0,0 +1,15 @@
+myList=range(15)
+for i in myList:
+    if i%2==0 and i%3==0:
+        print ("divisible by 2 and 3 are {}".format(i))
+
+        
+myList1=range(50-100)
+for i in myList1:
+    if i%5==0 or i%10==0:
+        print ("divisible by 5 or 10 are {}".format(i))
+
+
+
+#why 2nd for not performing ???
+        
